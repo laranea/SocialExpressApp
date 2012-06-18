42328e330eea9b26218ff893d44b30129ec29372
Kristof Leroux <kristof.leroux@gmail.com>
2012-06-18 02:16:08 +0200

    Merge with new UI.

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
-from kiss.core.application import Application
-from settings import options
+import tornado.ioloop
+import tornado.web
+from  tornado.template import Loader
+from tornado.web.UIModule import css_files
 
 
-app = Application(options)
-app.start()
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        #self.write("Hello, world")
+        loader = tornado.template.Loader("/home/ubuntu/socialexpress/views/templates")
+        self.write(loader.load("index.html").generate(error="hello"))
 
+application = tornado.web.Application([
+    (r"/", MainHandler),
 
+])
+
+if __name__ == "__main__":
+    application.listen(80)
+    tornado.ioloop.IOLoop.instance().start()
